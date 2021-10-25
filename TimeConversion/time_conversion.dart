963095d4eb5c838fa9cafebe5aafb554de0dbb03
ShabnaSirajudeen <shabna.sirajudeen@lxisoft.com>
2021-10-25 21:53:28 +0530

    Day 1 Exercise

---
+++ TimeConversion/time_conversion.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+
+void main() {
+  print('Enter Time in minutes: ');
+  int? time = int.parse(stdin.readLineSync()!);
+  print('Select the mode of conversion');
+  print('1. Hours');
+  print('2. Minutes');
+  print('3. Seconds');
+
+  int? mode = int.parse(stdin.readLineSync()!);
+  switch (mode) {
+    case 1:
+      double hours = time / 60;
+      print('${time} minutes is ${hours} hours');
+      break;
+    case 2:
+      print('${time} minutes');
+      break;
+    case 3:
+      int seconds = time * 60;
+      print('${time} minutes is ${seconds} seconds');
+      break;
+    default:
+      print('Invalid Option');
+  }
+}
