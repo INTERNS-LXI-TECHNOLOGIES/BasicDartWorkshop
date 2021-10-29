9642453e1953fa1cc8e89f1c93179d31176e3f7b
ShabnaSirajudeen <shabna.sirajudeen@lxisoft.com>
2021-10-29 19:50:27 +0530

    Exercise

---
+++ Day 4/Interest/interest.dart
@@ -0,0 +1,14 @@
+import 'dart:math';
+
+void main() {
+  double p = 1000000, r = 0.125, n = 4, t = 10;
+  double simpleInterest = p * r * t;
+  double cTotal = p * pow((1 + r / n), n * t);
+  double compoundInterest = cTotal - p;
+  double sTotal = p + simpleInterest;
+  print('Principal\tInterest\t\t\tTotal\t\t\tInterest Type');
+  print(
+      '----------------------------------------------------------------------------------------');
+  print('$p\t$simpleInterest\t\t\t$sTotal\t\tSimple Interest');
+  print('$p\t$compoundInterest\t\t$cTotal\tCompound Interest');
+}
