4ba0ed488c0bbb8367f2d569c5c9897e63468da8
sreejith.s <sreejith.s@lxisoft.com>
2021-12-24 17:22:33 +0530

    http://34.87.37.122:8080/browse/IN-19769

---
+++ Day 1/largest.dart
@@ -0,0 +1,17 @@
+void main() {
+  int n1 = 1;
+  int n2 = 3;
+  int n3 = 30;
+  int n4 = 15;
+  if (n1 > n2) {
+    print("n1=max");
+  } else if (n2 > n3) {
+    print("n2=max");
+  } else if (n3 > n4) {
+    n3 >= n4;
+    print("n3=max");
+  } else {
+    (n4 > n1);
+    print("n4=max");
+  }
+}
