4795e395e70c06904c6b5d5937728932877074fe
Kiran <kiran.c.k@lxisoft.com>
2022-04-28 19:36:15 +0530

    secondcommit

---
+++ Day 1/years.dart
@@ -0,0 +1,11 @@
+void main()
+{
+int years, months, weeks, days, hours, minutes;
+print("Enter years:");
+months=dayss/30;
+days=years/365;
+weeks=days/7;
+print("Days to years %d",years);
+print("\nDays to weeks %d",weeks);
+print("\nDays to months %d",months);
+}
