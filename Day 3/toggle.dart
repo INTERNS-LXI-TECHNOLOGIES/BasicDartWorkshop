32672370c8d3f6975b94a9a5132ce22ed5894800
Kiran <kiran.c.k@lxisoft.com>
2022-05-04 16:58:28 +0530

    lowercase

---
+++ Day 3/toggle.dart
@@ -0,0 +1,8 @@
+import 'dart:io';
+void main()
+{
+  String mainString;
+  print('Enter a String :');
+  mainString = stdin.readLineSync()!;
+  print('chnging the string in to lowercase : ${mainString.toLowerCase()}');
+}
